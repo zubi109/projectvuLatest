49fe666b433e58aa09b811476d7deb1d0dd9e1bf
Manzar Gulzar <manzar.gulzar@sumatosolutions.com>
2021-04-23 17:08:32 +0500

    question type validation on finish button of create question page

--- lib/teacher/CreateQuestion.dart
+++ lib/teacher/CreateQuestion.dart
@@ -274,19 +274,8 @@ setState(() {
                                 // primary: Colors.amber, // background
                                   onPressed: () {
                                     if (_formKey.currentState.validate()) {
-                                      if(quesiontype != QuestionType.MCQ.toString().split('.').last){
-                                        ScaffoldMessenger.of(context)
-                                            .showSnackBar(SnackBar(
-                                          content: Text('Creating Quiz...'),
-                                          backgroundColor: Colors.amber,
-                                        ));
-                                        finishQuiz();
-                                      }
-                                      else{
-                                        if(newQuestion.answer == _option1Controller.text ||
-                                            newQuestion.answer == _option2Controller.text ||
-                                            newQuestion.answer == _option3Controller.text ||
-                                            newQuestion.answer == _option4Controller.text){
+                                      if(quesiontype != null){
+                                        if(quesiontype != QuestionType.MCQ.toString().split('.').last){
                                           ScaffoldMessenger.of(context)
                                               .showSnackBar(SnackBar(
                                             content: Text('Creating Quiz...'),
@@ -295,11 +284,30 @@ setState(() {
                                           finishQuiz();
                                         }
                                         else{
-                                          Fluttertoast.showToast(
-                                              msg: "Please select answer from available 4 options!",
-                                              backgroundColor: Colors.red
-                                          );
+                                          if(newQuestion.answer == _option1Controller.text ||
+                                              newQuestion.answer == _option2Controller.text ||
+                                              newQuestion.answer == _option3Controller.text ||
+                                              newQuestion.answer == _option4Controller.text){
+                                            ScaffoldMessenger.of(context)
+                                                .showSnackBar(SnackBar(
+                                              content: Text('Creating Quiz...'),
+                                              backgroundColor: Colors.amber,
+                                            ));
+                                            finishQuiz();
+                                          }
+                                          else{
+                                            Fluttertoast.showToast(
+                                                msg: "Please select answer from available 4 options!",
+                                                backgroundColor: Colors.red
+                                            );
+                                          }
                                         }
+                                      }
+                                      else{
+                                        Fluttertoast.showToast(
+                                            msg: "Please select question type from available options!",
+                                            backgroundColor: Colors.red
+                                        );
                                       }
                                     }
                                   },
